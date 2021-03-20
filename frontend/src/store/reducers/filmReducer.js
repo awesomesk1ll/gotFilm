1e5e9c108d343a054e373fc8a109c3a6e9436772
Vlad Kovalev <vladsmit88@gmail.com>
2021-03-20 13:27:20 +0300

    update project, add store, edit js-files and components

---
+++ frontend/src/store/reducers/filmReducer.js
@@ -0,0 +1,19 @@
+import update from 'react-addons-update';
+import { FIND_FILM } from '../actions/filmActions';
+
+const initStore = {
+    films: []
+}
+
+export default function filmReducer(store = initStore, action) {
+    switch (action.type) {
+        case FIND_FILM: {
+            return update(store, {
+                films: [...store.films]
+            });
+        }
+        default:
+            return store;
+    }
+}
+
