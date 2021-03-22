4d0ad3e03a57c987c6a891e57f68a54c471a7ea8
Alexander <alex_sh_v@mail.ru>
2021-03-22 20:56:22 +0300

    added node-sass, change theme button

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import ThemeButton from '../components/ThemeButton';
+import MainPage from '../pages/MainPage';
 
 const App = (props) => {
   return (
     <div className="App">
-      Hello World!
+      <ThemeButton />
+      <MainPage />
     </div>
   );
 };
