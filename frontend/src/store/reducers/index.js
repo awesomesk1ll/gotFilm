248c1d67596083131e6ee66fc56b86e0bcb30d4b
Alexander <alex_sh_v@mail.ru>
2021-03-27 11:58:48 +0300

    redux-form deleted

--- frontend/src/store/reducers/index.js
+++ frontend/src/store/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
-import { reducer as formReducer } from 'redux-form'
 
 import filmReducer from './filmReducer';
 
 export default history => combineReducers({
     router: connectRouter(history),
     filmReducer,
-    form: formReducer,
 });
