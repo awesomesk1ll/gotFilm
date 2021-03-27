8d98569c3ddcbe1520df0e40e5f79abc58551aa8
Alexander <alex_sh_v@mail.ru>
2021-03-27 11:49:48 +0300

    added home

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import ThemeButton from '../components/ThemeButton';
-import MainPage from '../pages/MainPage';
+import Home from '../pages/Home/Home';
 
 const App = (props) => {
   return (
     <div className="App">
-      <ThemeButton />
-      <MainPage />
+      <Home />
     </div>
   );
 };
