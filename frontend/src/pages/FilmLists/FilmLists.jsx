d16ebc6aac88910895978272a8940580c3d8333f
Alexander <alex_sh_v@mail.ru>
2021-04-01 20:08:14 +0300

    finished film-lists

--- frontend/src/pages/FilmLists/FilmLists.jsx
+++ frontend/src/pages/FilmLists/FilmLists.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import Navbar from "../../components/Navbar/Navbar";
 import "./FilmLists.scss";
 
@@ -6,6 +8,30 @@ const FilmLists = () => {
   return (
     <div>
       <h1 className="filmLists__title">списки фильмов</h1>
+      <div className="filmLists__item">
+        <Link className="filmLists__link" to="/history">
+          История предложений
+        </Link>
+        24
+      </div>
+      <div className="filmLists__item">
+        <Link className="filmLists__link" to="/watched">
+          Просмотренные фильмы
+        </Link>
+        12
+      </div>
+      <div className="filmLists__item">
+        <Link className="filmLists__link" to="/rejectув">
+          Отклонённые фильмы
+        </Link>
+        7
+      </div>
+      <div className="filmLists__item">
+        <Link className="filmLists__link" to="/favorites">
+          Избранные
+        </Link>
+        3
+      </div>
       <Navbar />
     </div>
   );
