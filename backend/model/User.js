ba14869ed22dff5695ac38a8eb9c15e3eb1a6382
Nikita <nik-bogachev@mail.ru>
2021-04-12 18:49:27 +0500

    update backend

--- backend/model/User.js
+++ backend/model/User.js
@@ -1,10 +1,12 @@
-  
-const {Schema, model, Types} = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
 const schema = new Schema({
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    nickname: {type: String, required: true, unique: true}
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  nickname: { type: String, required: true, unique: true },
+  wishlist: { type: Array },
+  blacklist: { type: Array },
+  partFilms: { type: Array },
 });
 
-module.exports = model('User', schema)
+module.exports = model('User', schema);
