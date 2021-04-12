f6c5a69859b5cfd33287e3d1217eeaf1f2ffe977
Vlad Kovalev <vladsmit88@gmail.com>
2021-04-12 11:18:53 +0300

    change logic in actions

--- frontend/src/store/actions/changeFilmAction.js
+++ frontend/src/store/actions/changeFilmAction.js
@@ -3,11 +3,16 @@ import { getRandomFilm } from './filmActions';
 export const changeFilm = () => {
     return (dispatch, getState) => {
         const { films, blacklistFilms, alreadySeenFilms } = getState().filmReducer;
-        let randomFilm = Math.round(Math.random() * ((films.length - 1) - 0) + 0);
-        let checkBlackFilm = blacklistFilms.find(item => item.id === films[randomFilm].id);
-        let checkSeenFilm = alreadySeenFilms.find(item => item.id === films[randomFilm].id);
-        if (checkBlackFilm === undefined && checkSeenFilm === undefined) {
-            dispatch(getRandomFilm(randomFilm));
-        }
+        let film, randomIndex;
+        do {
+            randomIndex = ~~(Math.random() * films.length);
+            film = films[randomIndex];
+        } while (
+            // eslint-disable-next-line
+            blacklistFilms.some(item => item.id === film.id) ||
+            // eslint-disable-next-line
+            alreadySeenFilms.some(item => item.id === film.id)
+        )
+        dispatch(getRandomFilm(randomIndex));
     }
 };
