a6746712eb5853037b061c26e2ad8dc74dadb98f
Алексей Схоменко <a@skhom.ru>
2021-04-14 04:28:46 +0700

    spinner reworks
    added blackout and changed style/color

--- frontend/src/components/Spinner/Spinner.jsx
+++ frontend/src/components/Spinner/Spinner.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 
 import './Spinner.scss';
 
 const Spinner = (props) => {
-    return <Spin className="loadingSpinner" />
+    return <Spin className="loadingSpinner" indicator={<LoadingOutlined spin />} />
 };
 
 export default Spinner;
