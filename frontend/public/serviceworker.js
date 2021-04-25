eb1496c6bfe9ab720e43360951a0ec6b822d2e47
Alexander Shiryaev <alex_sh_v@mail.ru>
2021-04-25 15:45:41 +0300

    404.html replaced by offline.html in sw

--- frontend/public/serviceworker.js
+++ frontend/public/serviceworker.js
@@ -18,7 +18,7 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match('404.html'));
+      return fetch(event.request).catch(() => caches.match('offline.html'));
     })
   );
 });
