7f3009acc8ba2e0d782217d7fa66e3415a1b8b8d
Vlad Kovalev <vladsmit88@gmail.com>
2021-04-28 16:20:22 +0300

    add containers: SeenList and Blacklist, update router

--- frontend/src/components/ListItem/ListItem.jsx
+++ frontend/src/components/ListItem/ListItem.jsx
@@ -6,18 +6,18 @@ import TrashBin from '../icons/TrashBin';
 import './ListItem.scss';
 
 const ListItem = (props) => {
-    //const { name, secondName, year, rate, age, genre } = props;
+    const { name, secondName, year, rate, age, genre } = props;
     return (
         <div className="list-item">
-            <div className="list-item_about">
-                <p className="list-item__name">Большой Лебовски<span className="list-item__year">(1995)</span></p>
-                <p className="list-item__second-name">Big Lebowski</p>
-                <p className="list-item__genre">комедия<span className="list-item__age">16+</span></p>
+            <div className="list-item__about">
+                <p className="list-item__name">{ name }<span className="list-item__year"> ({ year })</span></p>
+                <p className="list-item__second-name">{ secondName }</p>
+                <p className="list-item__genre">{ genre }<span className="list-item__age">{ age }+</span></p>
             </div>
             <div className="list-item_actions-container">
                 <div className="list-item_rating">
                     <Star />
-                    <span className="list-item__rate">8.6</span>
+                    <span className="list-item__rate">{ rate }</span>
                 </div>
                 <div className="list-item__buttons">
                     <button><Bookmark /></button>
--- frontend/src/pages/Lists/Lists.jsx
+++ frontend/src/pages/Lists/Lists.jsx
@@ -12,8 +12,8 @@ const Lists = ({ blacklistFilms, alreadySeenFilms, nextTime, favorite }) => {
         <div className="lists--wrapper theme">
             <div className="lists__header">Списки фильмов</div>
             <Link className="lists__link--color" to="/film"><ListLink listLength={nextTime.data.length}>История предложений</ListLink></Link>
-            <Link className="lists__link--color" to="/film"><ListLink listLength={alreadySeenFilms.data.length}>Просмотренные фильмы</ListLink></Link>
-            <Link className="lists__link--color" to="/film"><ListLink listLength={blacklistFilms.data.length}>Отклоненные фильмы</ListLink></Link>
+            <Link className="lists__link--color" to="/seenList"><ListLink listLength={alreadySeenFilms.data.length}>Просмотренные фильмы</ListLink></Link>
+            <Link className="lists__link--color" to="/blacklist"><ListLink listLength={blacklistFilms.data.length}>Отклоненные фильмы</ListLink></Link>
             <Link className="lists__link--color" to="/film"><ListLink listLength={favorite.data.length}>Избранные</ListLink></Link>
             <Navigation checked={'lists'} />
         </div>
