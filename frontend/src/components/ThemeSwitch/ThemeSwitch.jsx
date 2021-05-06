73fdf2bee5476d7229f313d07806c50dd6fa10ff
Alexander Shiryaev <alex_sh_v@mail.ru>
2021-05-06 10:03:51 +0300

    theme-bag-fixed

--- frontend/src/components/ThemeSwitch/ThemeSwitch.jsx
+++ frontend/src/components/ThemeSwitch/ThemeSwitch.jsx
@@ -4,32 +4,23 @@ import { Switch } from 'antd';
 import { changeTheme } from '../../store/actions/themeAction';
 
 const ThemeSwitch = ({ isLightTheme }) => {
-  // const onChange = (checked) => {
-  //   document.body.classList.toggle('dark', checked);
-  // }
-
   const dispatch = useDispatch();
 
-  const onChange = (checked) => {
-    isLightTheme 
-      ? document.body.classList.add('dark', checked) 
-      : document.body.classList.remove('dark')
+  const onChange = (isLightTheme, checked) => {
+    document.body.classList.toggle('dark', isLightTheme, checked);
     dispatch(changeTheme(isLightTheme));
-  };
-
-  
+  }
 
   return (
       <Switch 
         onChange={onChange} 
+        checked={!isLightTheme && true}
       />
     );
 };
 
-const mapStateToProps = ({ themeReducer }) => {
-  return {
+const mapStateToProps = ({ themeReducer }) => ({
     isLightTheme: themeReducer.isLightTheme,
-  };
-};
+});
 
 export default connect(mapStateToProps, { changeTheme })(ThemeSwitch);
