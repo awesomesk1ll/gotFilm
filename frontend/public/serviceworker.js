09a57247912db176db9c746b90a70a60d0f5e5c3
Схоменко Алексей <a@skhom.ru>
2021-05-15 17:20:48 +0700

    service worker rewritten

--- frontend/public/serviceworker.js
+++ frontend/public/serviceworker.js
@@ -1,42 +1,79 @@
-const CACHE_NAME = 'version-1';
-const urlsToCache = ['index.html', 'offline.html'];
-
+const staticAppCacheName = 's-gf-app-rev0001';
+const dynamicAppCacheName = 'd-gf-app-rev0001';
+const filmsCacheName = 'gf-films-rev0001';
 const self = this;
 
-// Install SW
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache');
+const staticFiles = ['index.html', 'icons/favicon.ico', 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap'];
 
-      return cache.addAll(urlsToCache);
-    })
-  );
+const FILMS_ORIGINS = ['http://localhost:3000', 'https://gotfilm.tk'];
+
+const getDynamicFiles = async () => {
+  const list = await fetch('./asset-manifest.json').then(data => data.json());
+  return Object.values(list.files);
+}
+const getFilesList = async () => {
+  const dynamicFiles = await getDynamicFiles();
+  dynamicFiles.forEach(file => console.log('file', file));
+  return [...staticFiles, ...dynamicFiles.map(path => (path[0] === '/') ? path.slice(1) : path)];
+}
+
+// Install SW
+self.addEventListener('install', async (event) => {
+  console.log('SW: install event fired');
+  const filesList = await getFilesList();
+  const cache = await caches.open(staticAppCacheName);
+  await cache.addAll(filesList)
 });
 
 // Listen for requests
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match('offline.html'));
-    })
-  );
+  event.respondWith(cacheFirstOrSave(event.request));
 });
 
 // Activate the SW
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [];
-  cacheWhitelist.push(CACHE_NAME);
+self.addEventListener('activate', async (event) => {
+  console.log('SW: activate event fired');
+  const whiteCacheNames = [staticAppCacheName, dynamicAppCacheName, filmsCacheName];
+  const cacheNames = await caches.keys();
 
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      )
-    )
-  );
+  cacheNames.filter((name) => !whiteCacheNames.includes(name)).map((name) => caches.delete(name))
 });
+
+// unused base strategy
+async function cacheFirst (request) {
+  const cached = await caches.match(request);
+  console.log('SW: cf', !!cached, request.method, request.url);
+  return cached ?? await fetch(request)
+}
+
+// strategy trying to get data from cache then if there is no data -> fetch data and put it to cache
+async function cacheFirstOrSave (request) {
+  let fetched, origin, cache, error, options;
+  const cached = await caches.match(request);
+
+  console.log('SW: cfs', !!cached, request.method, request.url);
+  
+  origin = new URL(request.url).origin;
+
+  const isFilmsRequest = FILMS_ORIGINS.includes(origin) && (/\/films\/\d*.json/).test(request.url);
+  const isPictureRequest = origin === 'https://st.kp.yandex.net';
+  options = isPictureRequest ? {redirect: 'follow', mode: 'no-cors'} : {};
+
+  if (!cached) {
+    if (!origin.includes('chrome-extension')) {
+      cache = await caches.open(isFilmsRequest ? filmsCacheName : dynamicAppCacheName);
+      try {
+        fetched = await fetch(request.url, options);
+      } catch (err) {
+        console.log('fetch error (offline?):', err);
+      }
+      if (isPictureRequest && !fetched) {
+        
+        return await fetch('./offline.jpg')
+      }
+      
+      fetched && cache.put(request.url, fetched);
+    }
+  }
+  return cached ?? await fetch(request.url, options)
+}
