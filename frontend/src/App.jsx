935a2c7f99a9b2ea975b234d40c039d8ec3e057e
Схоменко Алексей <a@skhom.ru>
2021-05-17 23:35:16 +0700

    reworked App startup render and Error
    1. now app rendering only when there is films
    2. in error case error page will be shown
    3. now first action to get films is getFilms (in false case runs fetchFilms)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,37 +4,42 @@ import connect from 'react-redux/es/connect/connect';
 import { bindActionCreators } from 'redux';
 
 import Router from './containers/Router';
-import { fetchFilms } from './store/actions/complexFilmActions';
+import Error from './pages/Error';
+import { getFilms } from './store/actions/complexFilmActions';
 
-const App = ({ fetchFilms, settings, filmsCount }) => {
+const App = ({ getFilms, settings, filmsCount, error }) => {
   useEffect(() => {
     if (!filmsCount) {
-      fetchFilms();
+      getFilms();
     }
-  }, [fetchFilms, filmsCount]);
+  }, [getFilms, filmsCount]);
 
   document.body.classList.toggle("dark", settings.dark);
 
   return (
     <div className="App">
       <div className="container theme">
-        <Router /> 
+       { error ? <Error text={error}/> : 
+            filmsCount ? <Router /> : <div></div>
+        }
       </div>
     </div>
   );
 };
 
 App.propTypes = {
-  fetchFilms: PropTypes.func,
+  error: PropTypes.string,
+  getFilms: PropTypes.func,
   settings: PropTypes.object,
   filmsCount: PropTypes.number
 };
 
 const mapStateToProps = ({ filmReducer }) => ({
   settings: filmReducer.settings,
-  filmsCount: filmReducer.films.length
+  filmsCount: filmReducer.films.length,
+  error: filmReducer.error
 });
 
-const mapDispatchToProps = dispatch => bindActionCreators({ fetchFilms }, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({ getFilms }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
