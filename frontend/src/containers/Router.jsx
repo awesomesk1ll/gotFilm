8277cfe3e79018669696a0c33d57bb2d9fc1a601
Схоменко Алексей <a@skhom.ru>
2021-05-17 10:32:53 +0700

    router reworked to work with navigation
    1. added navigation
    2. routes now listed in array
    3. nav selection now depends on current route path

--- frontend/src/containers/Router.jsx
+++ frontend/src/containers/Router.jsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter } from 'react-router-dom';
 import { CSSTransition } from 'react-transition-group';
 
 import Home from "../pages/Home/Home";
+import Navigation from "../components/Navigation";
 import LoginForm from '../pages/LoginPage';
 import RegistrationForm from "../pages/RegistrationPage";
 import FilmCardContainer from './FilmCardContainer';
@@ -10,36 +11,39 @@ import Settings from '../pages/Settings';
 import Lists from '../pages/Lists';
 import {SeenList, History, Blacklist, Temporary, Favorites} from './List';
 
+const getSelection = (path) => (path === 'film' || path === 'settings') ? path : 'lists';
+
 const routes = [
-        { path: '/',             Component: Home                 , exact: true},
-        { path: "/login",        Component: LoginForm            },
-        { path: "/registration", Component: RegistrationForm     },
-        { path: "/film",         Component: FilmCardContainer    },
-        { path: "/settings",     Component: Settings             },
-        { path: "/lists",        Component: Lists                },
-        { path: "/history",      Component: History              },
-        { path: "/seenList",     Component: SeenList             },
-        { path: "/blacklist",    Component: Blacklist            },
-        { path: "/temporary",    Component: Temporary            },
-        { path: "/favorites",    Component: Favorites            }
+        { path: '/',             Component: Home                                    , exact: true  },
+        { path: "/login",        Component: LoginForm                               },
+        { path: "/registration", Component: RegistrationForm                        },
+        { path: "/film",         Component: FilmCardContainer    , showNav: true    },
+        { path: "/settings",     Component: Settings             , showNav: true    },
+        { path: "/lists",        Component: Lists                , showNav: true    },
+        { path: "/history",      Component: History              , showNav: true    },
+        { path: "/seenList",     Component: SeenList             , showNav: true    },
+        { path: "/blacklist",    Component: Blacklist            , showNav: true    },
+        { path: "/temporary",    Component: Temporary            , showNav: true    },
+        { path: "/favorites",    Component: Favorites            , showNav: true    },
     ];
 
-const Router = (props) => {
-    return (
-        <BrowserRouter>
-            {routes.map(({ path, Component, exact = false }) => (
-                <Route key={ path } exact path={ path }>
-                    {({ match }) => (
-                        <CSSTransition in={match != null} timeout={2000} classNames="page" unmountOnExit>
+const Router = (props) => (
+    <BrowserRouter>
+        { routes.map(({ path, Component, exact = false, showNav = false }) => (
+            <Route key={ path } exact={ exact } path={ path }>
+                {({ match }) => (
+                    <>
+                        <CSSTransition in={match != null} timeout={700} classNames="page" unmountOnExit>
                             <div className="page">
                                 <Component />
                             </div>
                         </CSSTransition>
-                    )}
-                </Route>
-            ))}
-        </BrowserRouter>
-    );
-};
+                        { (match && showNav) && <Navigation selection={ getSelection(path.slice(1)) } />}
+                    </>
+                )}
+            </Route>
+        )) }
+    </BrowserRouter>
+)
 
 export default Router;
